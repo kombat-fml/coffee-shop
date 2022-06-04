7d6162395462c4ec95368f5de1f28da12986a0e3
kombat-fml <informativpeople@gmail.com>
2022-06-04 09:03:26 +0300

    Add smooth scroll and form data-model

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -17,7 +17,7 @@
             <div class="title mt-5">Tell us about your tastes</div>
             <img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo" />
 
-            <form action="#" class="mt-5">
+            <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
                 <div class="col col-12 col-sm-3 d-flex align-items-center">
                   <label for="name-input" class="mb-0">
@@ -26,7 +26,7 @@
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="text" class="form-control" id="name-input" />
+                  <input v-model="form.name" type="text" class="form-control" id="name-input" />
                 </div>
               </div>
 
@@ -38,7 +38,7 @@
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="email" class="form-control" id="email-input" />
+                  <input v-model="form.email" type="email" class="form-control" id="email-input" />
                 </div>
               </div>
 
@@ -47,7 +47,7 @@
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="tel" class="form-control" id="phone-input" />
+                  <input v-model="form.phone" type="tel" class="form-control" id="phone-input" />
                 </div>
               </div>
 
@@ -60,6 +60,7 @@
                 </div>
                 <div class="col col-12">
                   <textarea
+                    v-model="form.textarea"
                     class="form-control"
                     name="message"
                     id="message"
@@ -71,7 +72,7 @@
 
               <div class="row">
                 <div class="col">
-                  <button class="btn btn-outline-dark send-btn">Send us</button>
+                  <button type="submit" class="btn btn-outline-dark send-btn">Send us</button>
                 </div>
               </div>
             </form>
@@ -92,6 +93,21 @@ export default {
     Navbar,
     ProductCard,
     PageTitle,
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        phone: '',
+        textarea: '',
+      },
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.form);
+    },
   },
 };
 </script>
--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -13,12 +13,12 @@
             <img class="beanslogo" src="@/assets/logo/Beans_logo.svg" alt="Beans logo" />
             <div class="preview__subtitle">We makes every day full of energy and taste</div>
             <div class="preview__subtitle">Want to try our beans?</div>
-            <a href="./coffeepage.html" class="preview__btn">More</a>
+            <a href="./coffeepage.html" class="preview__btn" @click.prevent="smoothScroll">More</a>
           </div>
         </div>
       </div>
     </div>
-    <section class="about">
+    <section class="about" id="about" ref="about">
       <div class="container">
         <div class="row">
           <div class="col-lg-6 offset-lg-3">
@@ -39,7 +39,7 @@
         </div>
       </div>
     </section>
-    <section class="best">
+    <section class="best" ref="best">
       <div class="container">
         <div class="title">Our best</div>
         <div class="row">
@@ -64,6 +64,8 @@
 import Navbar from '@/components/Navbar.vue';
 import ProductCard from '@/components/ProductCard.vue';
 import PageTitle from '@/components/PageTitle.vue';
+
+import { scrollIntoView } from 'seamless-scroll-polyfill';
 
 export default {
   components: {
@@ -94,6 +96,11 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    smoothScroll() {
+      scrollIntoView(this.$refs.best, { behavior: 'smooth', block: 'start' });
+    },
   },
 };
 </script>
